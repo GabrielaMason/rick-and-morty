e342fdbd12fa97b89a9ce720569fb7fede1fb987
GabrielaMason <0202578@up.edu.mx>
2025-12-01 08:26:04 -0600

    Like button for each card

--- components/CharacterCard/CharacterCard.tsx
+++ components/CharacterCard/CharacterCard.tsx
@@ -2,6 +2,8 @@
 
 import { Character } from "@/services/rickAndMortyApi";
 import styles from "./CharacterCard.module.css";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { addToFavorites, removeFromFavorites } from "@/store/FavoritesSlice";
 
 interface Props {
   character: Character;
@@ -10,6 +12,19 @@ interface Props {
 }
 
 export default function CharacterCard({ character, isSelected, onClick }: Props) {
+  const dispatch = useAppDispatch();
+  const favorites = useAppSelector((state) => state.favorites.items);
+  const isFavorite = favorites.some((fav) => fav.id === character.id);
+
+  const handleToggleFavorite = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    if (isFavorite) {
+      dispatch(removeFromFavorites(character.id));
+    } else {
+      dispatch(addToFavorites(character));
+    }
+  };
+
   return (
     <article
       className={`${styles.card} ${isSelected ? styles.cardSelected : ""}`}
@@ -23,6 +38,15 @@ export default function CharacterCard({ character, isSelected, onClick }: Props)
         alt={character.name}
         className={styles.image}
       />
+      <button
+        type="button"
+        className={`${styles.heart} ${isFavorite ? styles.heartActive : ""}`}
+        onClick={handleToggleFavorite}
+        aria-label={isFavorite ? "Remove from favorites" : "Add to favorites"}
+      >
+        {isFavorite ? "♥" : "♡"}
+        <span className={styles.likeText}>Like</span>
+      </button>
     </article>
   );
 }
